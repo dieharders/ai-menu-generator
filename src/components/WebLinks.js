e585f4e512e2b42e363953d8405b69fcefc86dca
DIEHARDERS <cybrobo@gmail.com>
2023-06-02 19:09:14 -0700

    Added a link

--- src/components/WebLinks.js
+++ src/components/WebLinks.js
@@ -1,7 +1,12 @@
 import styles from './WebLinks.module.scss';
 
+const companyId = '0';
+
 const WebLinks = () => {
-    return <div className={styles.container}></div>
+    return <div className={styles.container}>
+        {/* This should be a shortlink */}
+        <a href={`https://www.menu.bee/?id=${companyId}`}>{`https://menu.bee/?id=${companyId}`}</a>
+    </div>
 };
 
 export default WebLinks;
