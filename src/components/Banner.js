483f8e2fd8eede8e841ff3563edcf308041fba16
DIEHARDERS <cybrobo@gmail.com>
2023-06-16 21:42:22 -0700

    Removed all logo from banner

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,12 +1,10 @@
-import Logo from "./Logo";
 import styles from "./Banner.module.scss";
 
-const Banner = ({title, logo}) => {
+const Banner = ({title}) => {
     const backgroundURL = "url(https://cdn.bhdw.net/im/sunrise-art-wallpaper-81329_w635.webp)";
 
     return <div className={styles.container} style={{"backgroundImage": backgroundURL}}>
         <h1 className={styles.name}>{title}</h1>
-        <Logo src={logo} />
     </div>;
 };
 
