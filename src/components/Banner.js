4e2ef9d560df22ef577935231d0141116eb69454
DIEHARDERS <cybrobo@gmail.com>
2023-06-17 14:38:42 -0700

    Fix banner layout size

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,6 +1,7 @@
 import styles from "./Banner.module.scss";
 
 const Banner = ({title}) => {
+    // Temp background image. Maybe use AI to generate unique for each company?
     const backgroundURL = "url(https://cdn.bhdw.net/im/sunrise-art-wallpaper-81329_w635.webp)";
 
     return <div className={styles.container} style={{"backgroundImage": backgroundURL}}>
