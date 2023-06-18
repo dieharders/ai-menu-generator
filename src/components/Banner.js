0d1bfe22b5b98d28e85d76904ad48fdfb2866373
DIEHARDERS <cybrobo@gmail.com>
2023-06-17 18:19:45 -0700

    Add company colors to banner. Set default banner image.

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -2,7 +2,7 @@ import styles from "./Banner.module.scss";
 
 const Banner = ({ title, backgroundURL }) => {
     // Temp background image. Maybe use AI to generate unique for each company?
-    const url = `url(${backgroundURL})`;
+    const url = backgroundURL?.length > 0 ? `url(${backgroundURL})` : 'url(https://cdn.bhdw.net/im/sunrise-art-wallpaper-81329_w635.webp)';
 
     return <div className={styles.container} style={{"backgroundImage": url}}>
         <h1 className={styles.name}>{title}</h1>
