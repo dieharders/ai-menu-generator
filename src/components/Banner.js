b9446b251af74b19103f207e621cd3c89d1bd16d
DIEHARDERS <cybrobo@gmail.com>
2023-06-18 13:09:37 -0700

    Fix banner crash

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,9 +1,8 @@
 import styles from "./Banner.module.scss";
 
 const Banner = ({ title, backgroundURL }) => {
-    const companyBg = require(`../assets/banners/${backgroundURL}`);
     const defaultImage = 'https://cdn.bhdw.net/im/sunrise-art-wallpaper-81329_w635.webp';
-    const path = backgroundURL?.length > 0 ? companyBg : defaultImage;
+    const path = backgroundURL?.length > 0 ? require(`../assets/banners/${backgroundURL}`) : defaultImage;
     const url = `url(${path})`;
 
     return <div className={styles.container} style={{"backgroundImage": url}}>
