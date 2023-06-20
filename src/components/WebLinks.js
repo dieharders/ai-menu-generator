2fa6bf97092977b3d0c2168affd229e051906da3
DIEHARDERS <cybrobo@gmail.com>
2023-06-20 11:58:39 -0700

    Fix up print syling. Set fixed height for print items.

--- src/components/WebLinks.js
+++ src/components/WebLinks.js
@@ -2,8 +2,8 @@ import styles from './WebLinks.module.scss';
 
 const WebLinks = ({link}) => {
     return <div className={styles.container}>
-        <p className={styles.companyTitle}>International Dish</p>
-        <h3 className={styles.description}>Web version of menu</h3>
+        <p className={styles.companyTitle}>International Dish Company</p>
+        <h3 className={styles.description}>Access web version for more info</h3>
         <a href={link} className={styles.linkText}>{link}</a>
     </div>
 };
