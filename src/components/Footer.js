ab7f2eceb58256a04e4b5b439f1359286475f81d
DIEHARDERS <cybrobo@gmail.com>
2023-06-21 22:20:38 -0700

    Fix url from print version

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,15 +4,16 @@ import styles from "./Footer.module.scss";
 
 const Footer = ({data}) => {
     const origin = 'https://idish.app';
-    const link = `${origin}/?id=${data?.companyId}`;
+    const realLink = `${origin}/?id=${data?.companyId}/&lang=en`;
+    const visualLink = `${origin}/?id=${data?.companyId}`;
     const image = data?.bannerImage;
     const bgUrl = image ? require(`../assets/banners/${image}`) : "";
 
     return (
         <div className={styles.container}>
             <div className={styles.background} style={{backgroundImage: `url(${bgUrl})`}}></div>
-            <WebLinks link={link} />
-            <QRLink link={link}/>
+            <WebLinks link={visualLink} />
+            <QRLink link={realLink}/>
         </div>
     )
 };
